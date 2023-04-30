efa40ebf173ac6f42069786a10bd7599cf15dd1e
shikhasitaula <129104368+shikhasitaula@users.noreply.github.com>
2023-04-30 11:51:54 -0700

    Added script to the PyPoll and modified PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,16 +1,23 @@
-#import different librabry to perform the methds.
+# Imports different libraries to perform methods.
 import math 
 import csv
 
-# assign the path to get the correct file from where it is stored
+# Assigns the path to get the correct file from where it is stored.
 csvpath = "./Resources/budget_data.csv" 
-# use of 'with open' to assess the file and read and return the data.
-# the delimiter indicates the next value for the row 
+
+# Opens new file for storing the result.
+file_write = open("./analysis/PyBank_result.txt", 'w')
+
+# The function below wrint output to both terminal and file once it is called.
+def print_to_terminal_and_file(file_open, text):
+    print(text)
+    file_open.write(text + "\n")
+    
+# Uses of 'with open' to assess the file and read and return the data.     
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
     csv_header = next(csvreader)
-    #separates the header from the group of data
-    print(f"csv header: {csv_header}")
+    # Separates the header from the rest of the data.
     month_count = 0
     total_profit_loss = 0
     change_in_profitloss= 0
@@ -18,21 +25,22 @@ with open(csvpath) as csvfile:
     new_list = []
     greatest_increase = -math.inf 
     greatest_decrease = math.inf  
-    print("Financial Analysis")
-    print("-------------------------------------------")
-    # uses for loop to iterate through each of the rows and saves each row separately
+    print_to_terminal_and_file(file_write,"Financial Analysis\n")
+    print_to_terminal_and_file(file_write,"-------------------------------------------\n")
+    # Uses 'for loop' to iterate through each of the rows and saves each row separately.
     for row in csvreader:
-        #appends each rows to the new list.
+        # Appends each row to the new list.
         new_list.append(row)
-        #counts the number of row to find the total months
+        # Counts the number of rows to find the total months.
         month_count = month_count + 1
-        #calculates total profit and loss over the entire period
+        # Calculates total profit and loss over the entire period
         total_profit_loss = total_profit_loss + int(row[1])
         change_in_profitloss_total = 0
-    print(f"Total months : {month_count}")
-    print(f"Total :  ${total_profit_loss}")
-    # here the logic loops thorough entire rows and finds out the average change in profit and loss
-    # then compairs the change to find out the greatest increase and decrease in profit and loss
+    print_to_terminal_and_file(file_write,f"Total months : {month_count}\n")
+    print_to_terminal_and_file(file_write,f"Total :  ${total_profit_loss}\n")
+    
+    # The logic loops through entire rows and finds out the average change in profit and loss
+    # Then the if condition compairs the change to find out the greatest increase and decrease in profit and loss
     for index in range(1, len(new_list), 1):
         previous_profitloss= new_list[index - 1]
         current_profitloss= new_list[index]
@@ -45,7 +53,9 @@ with open(csvpath) as csvfile:
         if change_in_profitloss < greatest_decrease:
             greatest_decrease = change_in_profitloss 
             data_of_change_in_profitloss_decrease = current_profitloss[0]
-    average_of_change_in_profitloss = change_in_profitloss_total / (len(new_list)-1)                                     
-    print(f"Average Change:  ${round(average_of_change_in_profitloss, 2 )}")
-    print(f"Greatest Increase in Profits  :  {data_of_change_in_profitloss_increase} (${greatest_increase})")
-    print(f"Greatest Decrease in Profits  :  {data_of_change_in_profitloss_decrease} (${greatest_decrease})")
+    average_of_change_in_profitloss = change_in_profitloss_total / (len(new_list)-1)   
+    # Calls "the print_to_terminal_and_file" functions to print logic both in the terminal and the text file.                                 
+    print_to_terminal_and_file(file_write, f"Average Change:  ${round(average_of_change_in_profitloss, 2 )}\n")
+    print_to_terminal_and_file(file_write, f"Greatest Increase in Profits  :  {data_of_change_in_profitloss_increase} (${greatest_increase})\n")
+    print_to_terminal_and_file(file_write, f"Greatest Decrease in Profits  :  {data_of_change_in_profitloss_decrease} (${greatest_decrease})\n")
+    file_write.close()
---
+++ PyPoll/main.py
@@ -0,0 +1,55 @@
+# Impots csv and math library.
+import csv
+import math
+# Opens text file named PyPoll_result.txt.
+file_write = open("./analysis/PyPoll_result.txt", 'w' )
+# Goes to the the folder where the csv file is saved.
+csvpath = "./Resources/election_data.csv"
+# Prints the logic to both terminal and text file.
+def print_in_terminal_and_text_file(file_open, text):
+    print(text)
+    file_open.write(f"{text}\n")
+# Uses of 'with open' to assess the file and read and return the data.  
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    csvheader = next(csvreader)
+    print_in_terminal_and_text_file(file_write,"Election Results\n")
+    print_in_terminal_and_text_file(file_write,"-------------------------------\n")
+    
+    total_vote_count = 0
+    # Creates the dictionaty and saves the unique candidate and stores the total vote counts of each.
+    vote_count_for_each_candidate = {}
+    candidate_count = 0
+    # Iterates though each row in the csv file.
+    # Counts the total number of votes cast.
+    # Uses if condition to iterate though each row in column 2 to find the unique candidate as key.
+    # counts the votes count of each candidate and stores it as a value.
+    for row in csvreader:
+        total_vote_count = total_vote_count + 1   
+        if (row[2] in vote_count_for_each_candidate):
+            candidate_count = vote_count_for_each_candidate[row[2]] + 1
+            vote_count_for_each_candidate[row[2]] = candidate_count   
+        else: 
+            candidate_count = 1
+            vote_count_for_each_candidate[row[2]] = candidate_count
+        percentage = (candidate_count/ total_vote_count)*100
+    print_in_terminal_and_text_file(file_write,f"Total Votes:  {total_vote_count}\n")
+    print_in_terminal_and_text_file(file_write,"-------------------------------\n")       
+    winner = -math.inf
+    winner_name = ""
+    # Iterates throung each candidate and performs the percentage calculation.
+    # Uses if condition to find the candidate with highest number of vote.
+    for candidate in vote_count_for_each_candidate:
+        vote_count =  vote_count_for_each_candidate[candidate]
+        percentage = round((vote_count/ total_vote_count)*100, 3)
+        print_in_terminal_and_text_file(file_write, f"{candidate} : {percentage}%  ({vote_count_for_each_candidate[candidate]})\n")
+        if  vote_count > winner:
+            winner = vote_count 
+            winner_name = candidate
+    print_in_terminal_and_text_file(file_write,"-------------------------------\n") 
+    print_in_terminal_and_text_file(file_write,f"Winner: {winner_name}\n")   
+    print_in_terminal_and_text_file(file_write,"-------------------------------") 
+# Close the text file     
+file_write.close() 
+   
+        
